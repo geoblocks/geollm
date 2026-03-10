d1eed016b484eafae3fa30f7a05b5b63805afce6
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-03-10 08:58:05 +0100

    feat: add CompositeDataSource to aggregate results from multiple GeoDataSource instances

--- geollm/datasources/__init__.py
+++ geollm/datasources/__init__.py
@@ -4,10 +4,14 @@ Geographic data source layer for resolving location names to geometries.
 Provides a Protocol-based interface for data sources and a SwissNames3D implementation.
 """
 
+from .composite import CompositeDataSource
+from .ign_bdtopo import IGNBDTopoSource
 from .protocol import GeoDataSource
 from .swissnames3d import SwissNames3DSource
 
 __all__ = [
+    "CompositeDataSource",
     "GeoDataSource",
+    "IGNBDTopoSource",
     "SwissNames3DSource",
 ]
